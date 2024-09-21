cb673fdea531bed2727c31bee1410a3fbd5f7fb6
kemelincristo <130564253+kemelincristo@users.noreply.github.com>
2024-09-20 22:18:35 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+constnumeroSenha= document.querySelector('parametro-senha_texto');
+lettamanhoSenha=12;
+numeroSenha.textContent=tamanhoSenha;
+constletrasMaíscula="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+c0nstletrasMinusculas="abcdefghijklmnopqrstuvwxyz"
+constnumero="0123456789";
+constsimbolo="!@%*?";
+constbotoes=document.query
