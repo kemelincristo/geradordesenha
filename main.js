0a41d00e264d75279fadf188af1235aadf5d4aaf
kemelincristo <130564253+kemelincristo@users.noreply.github.com>
2024-09-27 22:05:52 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -5,4 +5,15 @@ constletrasMaíscula="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 c0nstletrasMinusculas="abcdefghijklmnopqrstuvwxyz"
 constnumero="0123456789";
 constsimbolo="!@%*?";
-constbotoes=document.query
+constbotoes=document.querySelectorAll("parametro-senha_botao");
+constcampoSenha=document.querySelector("campo-senha");
+constcheckbox= document.querySelectorAll("checkbox");
+constforcaSenha=Document.querySelector("forca");
+botoes[0].onclick=diminuiTamanho;
+botoes[1].onclick=aumentaTamanho;
+functiondiminuiTamanho(){
+    
+}
+
+
+
